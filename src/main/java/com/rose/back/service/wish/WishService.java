82897367e0c60e330812990f382bc95a9ba32f3b
50104 <solaya7@naver.com>
2024-05-27 20:26:51 +0900

    리액트 연동 설정(cors 설정, 연동테스트 완료)

---
+++ src/main/java/com/rose/back/service/wish/WishService.java
@@ -0,0 +1,9 @@
+// package com.rose.back.service.wish;
+
+// import org.springframework.data.jpa.repository.JpaRepository;
+
+// import com.rose.back.entity.TestEntity;
+
+// public interface WishService extends JpaRepository<TestEntity, String> {
+    
+// }
