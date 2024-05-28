65084138fa34cd32d9200c2ce3521dbd4e0cba05
50104 <solaya7@naver.com>
2024-05-28 19:37:20 +0900

    공통 타입 선언

---
+++ src/main/java/com/rose/back/common/ResponseMessage.java
@@ -0,0 +1,14 @@
+package com.rose.back.common;
+
+public interface ResponseMessage {
+    
+    String SUCCESS = "Success";
+
+    String VALIDATION_FAIL = "Validation Failed";
+    String DUPLICATE_ID = "Duplicate Id";
+
+    String SIGN_IN_FAIL = "Login information mismatch";
+    String CERTIFICATION_FAIL = "Certification Failed";
+
+    String DATABASE_ERROR = "Database error";
+}
