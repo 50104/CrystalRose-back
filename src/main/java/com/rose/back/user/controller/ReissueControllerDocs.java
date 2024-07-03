9b1db440740008f45b4a7983efd5ea103fe0dfab
50219 <parksol61@gmail.com>
2024-07-04 04:53:13 +0900

    Feat : 오어스 로그인 토큰 저장 구현 (refresh 토큰으로 access 토큰 재발급)

---
+++ src/main/java/com/rose/back/user/controller/ReissueControllerDocs.java
@@ -0,0 +1,19 @@
+package com.rose.back.user.controller;
+
+import org.springframework.http.ResponseEntity;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface ReissueControllerDocs {
+
+    @Operation(summary = "리프레쉬 토큰 불러오기", description = "리프레쉬 토큰을 불러옵니다")
+    @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "불러오기 성공"),
+    @ApiResponse(responseCode = "409", description = "불러오기 실패")})
+    public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response);
+    
+}
