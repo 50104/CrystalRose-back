fb425db612e98eb0914b166e917594f0e0a192a9
50104 <solaya7@naver.com>
2025-05-27 15:29:42 +0900

    Feat : Swagger 설정에 개발 서버 url 및 설명 추가

--- src/main/java/com/rose/back/config/docs/SwaggerConfig.java
+++ src/main/java/com/rose/back/config/docs/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package com.rose.back.config.docs;
 
+import java.util.List;
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,11 +14,19 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
-        Info info = new Info()
-                .version("v1.0")
-                .title("CristalRose API")
-                .description("빛나는 크리스퇄 가드닝 API");
+
         return new OpenAPI()
-                .info(info);
+                .info(new Info()
+                        .version("v1.0.0")
+                        .title("CristalRose API")
+                        .description("빛나는 크리스퇄 가드닝 API")
+                )
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:4000")
+                                .description("개발용 서버")
+                        )
+                );
     }
+    
 }
