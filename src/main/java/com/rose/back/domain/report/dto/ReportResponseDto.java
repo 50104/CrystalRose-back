70cd07b4f5ce122700782d6309791e25ad788ad9
50104 <solaya7@naver.com>
2025-08-13 20:31:45 +0900

    feat: 게시글 신고 내역 해당 글 상세 페이지 이동

--- src/main/java/com/rose/back/domain/report/dto/ReportResponseDto.java
+++ src/main/java/com/rose/back/domain/report/dto/ReportResponseDto.java
@@ -6,6 +6,7 @@ import com.rose.back.domain.report.entity.Report;
 
 public record ReportResponseDto(
     Long reportId,
+    Long boardNo,
     String postTitle,  
     String postContent,
     String reason,
@@ -16,6 +17,7 @@ public record ReportResponseDto(
     public static ReportResponseDto from(Report report) {
         return new ReportResponseDto(
             report.getId(),
+            report.getTargetPost().getBoardNo(),
             report.getTargetPost().getBoardTitle(),
             report.getTargetPost().getBoardContent(),
             report.getReason(),
