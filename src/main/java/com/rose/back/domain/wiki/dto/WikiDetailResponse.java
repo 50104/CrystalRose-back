4f61beee1599d769725a476ba1c6ceb939c8ce95
50104 <solaya7@naver.com>
2025-09-13 17:37:37 +0900

    feat: 도감 거절 사유

--- src/main/java/com/rose/back/domain/wiki/dto/WikiDetailResponse.java
+++ src/main/java/com/rose/back/domain/wiki/dto/WikiDetailResponse.java
@@ -27,4 +27,5 @@ public class WikiDetailResponse {
     private String status;
     private String modificationStatus;
     private LocalDateTime createdDate;
+    private String rejectionReason;
 }
