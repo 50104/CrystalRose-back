52f4d65a847b2a6480177595fe9291928c2e3714
50104 <solaya7@naver.com>
2025-09-23 15:49:25 +0900

    feat: 거절된 도감 수정 제출 마감일 계산 추가

--- src/main/java/com/rose/back/domain/wiki/dto/WikiModificationListDto.java
+++ src/main/java/com/rose/back/domain/wiki/dto/WikiModificationListDto.java
@@ -1,6 +1,9 @@
 package com.rose.back.domain.wiki.dto;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 
 import com.rose.back.domain.wiki.entity.WikiModificationRequest;
 
@@ -10,16 +13,30 @@ public record WikiModificationListDto(
     String name,
     String description,
     LocalDateTime createdDate,
-    WikiModificationRequest.Status status
+    WikiModificationRequest.Status status,
+    Long remainingDays
 ) {
     public static WikiModificationListDto from(WikiModificationRequest entity) {
+        Long remainingDays = null;
+        
+        // 삭제 예정 REJECTED
+        if (entity.getStatus() == WikiModificationRequest.Status.REJECTED && entity.getModifiedDate() != null) {
+            LocalDate rejectedDate = entity.getModifiedDate().toLocalDate();
+            LocalDate deletionDate = rejectedDate.plusDays(4);
+            LocalDateTime deletionScheduledDate = deletionDate.atTime(LocalTime.MIDNIGHT);
+            
+            long hoursBetween = ChronoUnit.HOURS.between(LocalDateTime.now(), deletionScheduledDate);
+            remainingDays = Math.max(0, hoursBetween / 24);
+        }
+        
         return new WikiModificationListDto(
             entity.getId(),
             entity.getOriginalWiki().getId(),
             entity.getName(),
             entity.getDescription(),
             entity.getCreatedDate(),
-            entity.getStatus()
+            entity.getStatus(),
+            remainingDays
         );
     }
 }
